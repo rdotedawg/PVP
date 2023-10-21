adccd40ceb15bc16dd264fe32947174da146a892
rDoteDawg <Default@PC>
2023-10-21 14:12:33 +0100

    Update

--- soma1/Mainform.Designer.cs
+++ soma1/Mainform.Designer.cs
@@ -64,7 +64,6 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.syrumspeed = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.mpslider)).BeginInit();
@@ -449,22 +448,11 @@
             this.syrumspeed.Size = new System.Drawing.Size(110, 21);
             this.syrumspeed.TabIndex = 22;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(375, 189);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 24;
-            this.button1.Text = "Click Me";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
             // Mainform
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(541, 257);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.syrumspeed);
             this.Controls.Add(this.label8);
@@ -539,7 +527,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox syrumspeed;
-        private System.Windows.Forms.Button button1;
     }
 }
 
